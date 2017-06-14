b0d73fe0c0df94d56cacfa6ead174f5c9ce9eb63
October Hammer <octoberhammer@gmail.com>
2017-06-14 21:40:38 +0300

    Initial Commit

---
+++ Eventbright/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Eventbright
+//
+//  Created by October Hammer on 6/14/17.
+//  Copyright © 2017 Ocotober Hammer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
