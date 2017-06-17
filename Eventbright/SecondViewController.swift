6a1a4c72f4e68b207ae8a9a46067d92a7c06f774
October Hammer <octoberhammer@gmail.com>
2017-06-17 10:47:42 +0300

    Для Первого вью контроллера - Навигэйшен Бар

--- Eventbright/SecondViewController.swift
+++ Eventbright/SecondViewController.swift
@@ -15,6 +15,8 @@ class SecondViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
+    @IBOutlet var arrayOfLabels: [UILabel]!
+    
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
